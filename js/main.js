724ab54f742add1b889854f00e6476ba94c39226
Murod <63477786+MurodDevUzb@users.noreply.github.com>
2021-04-14 22:13:30 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,54 +8,54 @@ burger.addEventListener('click', ()=>{
         body.classList.toggle('lock');
 })
 
-$(".head-carousel").owlCarousel({
-        autoplay: true,
-        loop: true,
-        items: 1,
-        nav:true,
-        navText: ["<img src='./img/button-left-img.png'>","<img src='./img/button-right-img.png'>"]
-});
+// $(".head-carousel").owlCarousel({
+//         autoplay: true,
+//         loop: true,
+//         items: 1,
+//         nav:true,
+//         navText: ["<img src='./img/button-left-img.png'>","<img src='./img/button-right-img.png'>"]
+// });
 
-$(".testimonials-carousel").owlCarousel({
-        items:1,
-        autoplay: true,
-        loop: true,
-        items: 1,
-        dots: true
-        // navText: ["<img src='./img/button-left-img.png'>","<img src='./img/button-right-img.png'>"]
-});
-var owl = $('.owl-carousel');
-owl.owlCarousel();
-// Go to the next item
-$('.customNextBtn').click(function() {
-    owl.trigger('next.owl.carousel');
-})
-// Go to the previous item
-$('.customPrevBtn').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [3000]);
-})
+// $(".testimonials-carousel").owlCarousel({
+//         items:1,
+//         autoplay: true,
+//         loop: true,
+//         items: 1,
+//         dots: true
+//         // navText: ["<img src='./img/button-left-img.png'>","<img src='./img/button-right-img.png'>"]
+// });
+// var owl = $('.owl-carousel');
+// owl.owlCarousel();
+// // Go to the next item
+// $('.customNextBtn').click(function() {
+//     owl.trigger('next.owl.carousel');
+// })
+// // Go to the previous item
+// $('.customPrevBtn').click(function() {
+//     // With optional speed parameter
+//     // Parameters has to be in square bracket '[]'
+//     owl.trigger('prev.owl.carousel', [3000]);
+// })
 
-$('.counter-up').counterUp({
-        delay: 10,
-        time: 1000
-});
-const el = document.querySelector('.counter-up')
-new Waypoint( {
-    element: el,
-    handler: function() { 
-      counterUp( el ) 
-      this.destroy()
-    },
-    offset: 'bottom-in-view'
-})
-
-// $('.counters').counterUp({
+// $('.counter-up').counterUp({
 //         delay: 10,
 //         time: 1000
 // });
-AOS.init();
+// const el = document.querySelector('.counter-up')
+// new Waypoint( {
+//     element: el,
+//     handler: function() { 
+//       counterUp( el ) 
+//       this.destroy()
+//     },
+//     offset: 'bottom-in-view'
+// })
+
+// // $('.counters').counterUp({
+// //         delay: 10,
+// //         time: 1000
+// // });
+// AOS.init();
 
 // $('.grid').isotope({
 //         itemSelector: '.grid-img',
